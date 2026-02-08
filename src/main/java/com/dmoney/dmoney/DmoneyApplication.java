90144862f628d1afacee5597530033ca93bbe055
Dupegui <dupegui2002@gmail.com>
2026-02-08 16:38:16 -0500

    Initial commit

---
+++ src/main/java/com/dmoney/dmoney/DmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.dmoney.dmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmoney/dmoney/DmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmoney.dmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
