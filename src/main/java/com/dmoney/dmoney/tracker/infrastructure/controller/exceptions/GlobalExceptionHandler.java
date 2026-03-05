279861998ceb6b7d61eac28ab9e13cab198cec02
Dupegui <dupegui2002@gmail.com>
2026-03-05 11:25:34 -0500

    Global exception handler completed

--- src/main/java/com/dmoney/dmoney/tracker/infrastructure/controller/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dmoney/dmoney/tracker/infrastructure/controller/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.dmoney.dmoney.tracker.infrastructure.controller.exceptions;
 
 import com.dmoney.dmoney.tracker.exceptions.ResourceAlreadyExistsException;
+import com.dmoney.dmoney.tracker.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +16,33 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(ApiError.of(HttpStatus.CONFLICT, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> handleNotFound(ResourceNotFoundException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiError.of(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleArgumentNotValid(MethodArgumentNotValidException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiError.of(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ApiError> handleNullPointerException(NullPointerException ex){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiError.of(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected Error"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGenericException(Exception ex){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiError.of(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected Error"));
     }
 }
