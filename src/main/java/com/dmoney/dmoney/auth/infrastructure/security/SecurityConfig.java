16e8c914917897951a3f00254f9056e121f47fb0
Dupegui <dupegui2002@gmail.com>
2026-07-24 12:49:08 -0500

    http authorization for the open api links

--- src/main/java/com/dmoney/dmoney/auth/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/dmoney/dmoney/auth/infrastructure/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         )
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
